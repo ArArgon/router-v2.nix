097a4f626998be56afccb00103072260fbd76acc
ArArgon <liaozping@gmail.com>
2025-12-20 00:43:56 -0800

    chore: refactor flake

--- flake.nix
+++ flake.nix
@@ -1,23 +1,30 @@
 {
   description = "Router configuration with NixOS";
+
+  # To use Tsinghua mirror, replace the nixpkgs.url in the main flake inputs with:
+  # nixpkgs.url = "git+https://mirrors.tuna.tsinghua.edu.cn/git/nixpkgs.git?ref=nixos-25.11";
+  # The inputs below will follow that nixpkgs.url automatically.
   inputs = {
-    # Tsinghua Nixpkgs mirror
-    nixpkgs.url = "git+https://mirrors.tuna.tsinghua.edu.cn/git/nixpkgs.git?ref=nixos-25.11";
-    nixpkgs-unstable.url = "git+https://mirrors.tuna.tsinghua.edu.cn/git/nixpkgs.git?ref=nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
     { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
     {
       nixosModules = {
         router-v2 = import ./modules {
-          pkgs = import nixpkgs { system = "x86_64-linux"; };
+          inherit pkgs;
         };
       };
 
       # This is just for testing/CI - doesn't affect the module export
       nixosConfigurations.test = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
+        inherit system;
         modules = [
           ./modules
           {
