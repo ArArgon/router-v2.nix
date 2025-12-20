c5dbf9b3b96f73c67c0511eaac29aba64a6b65a3
ArArgon <liaozping@gmail.com>
2025-12-19 22:18:01 -0800

    fix: remove 'system' from inputs

--- flake.nix
+++ flake.nix
@@ -1,18 +1,17 @@
 {
   description = "Router configuration with NixOS";
   inputs = {
-    system = "x86_64-linux";
     # Tsinghua Nixpkgs mirror
     nixpkgs.url = "git+https://mirrors.tuna.tsinghua.edu.cn/git/nixpkgs.git?ref=nixos-25.11";
     nixpkgs-unstable.url = "git+https://mirrors.tuna.tsinghua.edu.cn/git/nixpkgs.git?ref=nixpkgs-unstable";
   };
 
   outputs =
-    { nixpkgs, system, ... }:
+    { nixpkgs, ... }:
     {
       nixosModules = {
         router-v2 = import ./modules {
-          pkgs = import nixpkgs { system = system; };
+          pkgs = import nixpkgs { system = "x86_64-linux"; };
         };
       };
     };
