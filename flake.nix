ab34454224a42c6e369e5fbd0622cf9af8fc15f6
ArArgon <liaozping@gmail.com>
2025-12-20 00:50:12 -0800

    fix: remove the problematic nixpkgs follows

--- flake.nix
+++ flake.nix
@@ -1,12 +1,8 @@
 {
   description = "Router configuration with NixOS";
 
-  # To use Tsinghua mirror, replace the nixpkgs.url in the main flake inputs with:
-  # nixpkgs.url = "git+https://mirrors.tuna.tsinghua.edu.cn/git/nixpkgs.git?ref=nixos-25.11";
-  # The inputs below will follow that nixpkgs.url automatically.
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-    nixpkgs.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
